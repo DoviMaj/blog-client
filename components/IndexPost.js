befd3f23780bc16a3755a2d95c13a83d511641f0
dovimaj <dovimajowka@gmail.com>
2021-02-08 00:41:58 +0200

    Post list style and date formated

--- components/IndexPost.js
+++ components/IndexPost.js
@@ -2,14 +2,34 @@ import React from "react";
 import style from "../styles/Post.module.scss";
 
 function Post({ post }) {
+  const date = new Date(post.date);
+  const date_formated = date.toLocaleDateString("en-gb", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  });
   return (
-    <div className={style.post} key={post._id}>
-      <a href={`/posts/${post._id}`}>
+    <li className={style.post} key={post._id}>
+      <a
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          zIndex: "0",
+          position: "relative",
+        }}
+        href={`/posts/${post._id}`}
+      >
         <h1>{post.author_name}</h1>
-        <a href={`/posts/${post._id}`}>{post.title}</a>
-        <p>{post.text}</p>
+        <p>{post.title}</p>
+        <p>{date_formated}</p>
+        <button style={{ position: "absolute", top: "195px", width: "100%" }}>
+          View Post
+        </button>
       </a>
-    </div>
+    </li>
   );
 }
 
