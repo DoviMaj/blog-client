e408ac6e88d14738307b780bb68df241d878d2de
dovimaj <dovimajowka@gmail.com>
2021-02-08 00:58:39 +0200

    Description mrta tag and lang=en

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Blog</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="Description" content="NextJS Blog website"></meta>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700;900&display=swap"
