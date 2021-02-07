e10e61a26d5a4bfc05fd87702342f7994bd20038
dovimaj <dovimajowka@gmail.com>
2021-02-08 00:42:34 +0200

    Clean up

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,42 +3,41 @@ import SinglePost from "../../components/SinglePost.tsx";
 
 interface Props {
   post: Object;
+  comments: Object;
 }
 
-const Post: React.FC<Props> = ({ post }) => {
-  return <SinglePost post={post} />;
+const Post: React.FC<Props> = ({ post, comments }) => {
+  return <SinglePost post={post} comments={comments} />;
 };
 
-// This function gets called at build time
 export const getStaticPaths: GetStaticPaths = async () => {
-  // Call an external API endpoint to get posts
   const res = await fetch("https://dovimaj-blog-api.herokuapp.com/api/posts");
   const resJson = await res.json();
   const posts = resJson.posts;
 
-  // Get the paths we want to pre-render based on posts
   const paths = posts.map((post: any) => `/posts/${post._id}`);
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
   return { paths, fallback: false };
 };
 
-// This also gets called at build time
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(
+  const req = await fetch(
     `https://dovimaj-blog-api.herokuapp.com/api/posts/${params.id}`
   );
-  const resJson = await res.json();
+  const resJson = await req.json();
   const post = resJson.post;
-  // Pass post data to the page via props
+
+  const commentsRes = await fetch(
+    `https://dovimaj-blog-api.herokuapp.com/api/posts/${params.id}/comments`
+  );
+
+  const commentsResJson = await commentsRes.json();
+  let comments = commentsResJson.comments;
+  if (!comments) {
+    comments = null;
+  }
   return {
-    props: { post },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every second
-    revalidate: 1, // In seconds
+    props: { post, comments },
+    revalidate: 1,
   };
 };
 
