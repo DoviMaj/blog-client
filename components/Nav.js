3d9c981f1713c9c596f749d2282d15c5a3ee5b41
dovimaj <dovimajowka@gmail.com>
2021-04-08 21:33:11 +0300

    Fix: next/link not working

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,10 @@
-import Link from "next/link";
 import React from "react";
 import styles from "../styles/Nav.module.scss";
 
 const Nav = () => {
   return (
     <nav className={styles.navbar}>
-      <ul>
-        <li>
-          <Link href="/">Blog</Link>
-        </li>
-      </ul>
+      <a href="/">Blog</a>
     </nav>
   );
 };
