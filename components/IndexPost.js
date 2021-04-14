7e5799854eaa096582adf181386e87137f78f75c
dovimaj <dovimajowka@gmail.com>
2021-04-14 03:17:10 +0300

    Update styles
    * support browsers that have no flex-gap support
    * display indexPost more nicely

--- components/IndexPost.js
+++ components/IndexPost.js
@@ -11,25 +11,16 @@ function Post({ post }) {
     minute: "2-digit",
   });
   return (
-    <li className={style.post} key={post._id}>
-      <a
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "20px",
-          zIndex: "0",
-          position: "relative",
-        }}
-        href={`/posts/${post._id}`}
-      >
+    <a href={`/posts/${post._id}`} className={style.post} key={post._id}>
+      <div>
         <h1>{post.author_name}</h1>
         <p>{post.title}</p>
-        <p>{date_formated}</p>
-        <button style={{ position: "absolute", top: "195px", width: "100%" }}>
-          View Post
-        </button>
-      </a>
-    </li>
+      </div>
+      <div>
+        <p style={{ marginBottom: "20px" }}>{date_formated}</p>
+        <button style={{ width: "100%" }}>View Post</button>
+      </div>
+    </a>
   );
 }
 
